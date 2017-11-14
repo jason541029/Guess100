65b9f78013e379d3270c5ca2f47aabc81cf95ff5
Jason541029 <jtr5494@gmail.com>
2017-11-14 10:42:26 +0800

    findViews

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -9,7 +9,25 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    String s = new String("abc");
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Please enter a number(1-100)")
+                    .setMessage("1-100")
+                    .setPositiveButton("SEND",null)
+                    .show();
 
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button SEND = (Button) findViewById(R.id.SEND);
+        SEND.setOnClickListener(listener);}
 
 
 
